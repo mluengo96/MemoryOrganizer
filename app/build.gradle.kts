48770b2c701ac622e1fc7a33ae2fe881a62ffb4b
Mario <marioluengo3d@gmail.com>
2024-09-18 21:12:25 +0200

    Added compose animation library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.compose.animation)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material)
     implementation(libs.emoji2.emojipicker)
