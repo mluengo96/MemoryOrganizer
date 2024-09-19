820d918a4bd8a5bfa5711758dbc9698da28e832c
Mario <marioluengo3d@gmail.com>
2024-09-19 19:38:01 +0200

    Refactor to FolderDetailScreen

--- app/src/main/java/com/mluengo/memoryorganizer/ui/screens/folders/FolderDetailScreen.kt
+++ app/src/main/java/com/mluengo/memoryorganizer/ui/screens/folders/FolderDetailScreen.kt
@@ -8,16 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.mluengo.memoryorganizer.ui.components.ItemCard
+import com.mluengo.memoryorganizer.R
+import com.mluengo.memoryorganizer.ui.components.cards.ItemCard
+import com.mluengo.memoryorganizer.ui.components.TopAppBar
 import com.mluengo.memoryorganizer.ui.theme.LocalSpacing
 import com.mluengo.memoryorganizer.ui.theme.MemoryOrganizerTypography
 
@@ -25,6 +31,7 @@ import com.mluengo.memoryorganizer.ui.theme.MemoryOrganizerTypography
 fun ItemScreen(
     navController: NavController,
     lazyListState: LazyListState,
+    isTopAppBarVisible: Boolean,
 ) {
     val spacing = LocalSpacing.current
     LaunchedEffect(Unit) {
@@ -36,6 +43,17 @@ fun ItemScreen(
             .fillMaxSize(),
         verticalArrangement = Arrangement.Top
     ) {
+        TopAppBar(
+            title = "Folder's name",
+            hasNavigationButton = true,
+            actionIcon = Icons.Rounded.MoreVert,
+            actionIconContentDescription = stringResource(id = R.string.edit),
+            onActionClick = { /* TODO */ },
+            navigationIconContentDescription = stringResource(id = R.string.back),
+            onNavigationClick = { navController.navigateUp() },
+            lazyListState = lazyListState,
+            isVisible = isTopAppBarVisible,
+        )
         Column {
             LazyColumn(
                 contentPadding = PaddingValues(spacing.spaceMedium),
@@ -51,9 +69,19 @@ fun ItemScreen(
                     Spacer(modifier = Modifier.height(spacing.spaceLarge))
                 }
 
+                val testLinks = listOf(
+                    "https://not-valid-url", // --> Invalid URL
+                    "https://m3.material.io/develop/android/jetpack-compose", // --> Valid URL
+                    "https://expatexplore.com/blog/when-to-travel-weather-seasons/", // --> URL that does not contain image
+                )
+
                 // Add 5 items
-                items(5) {
-                    ItemCard()
+                items(testLinks) { link ->
+                    ItemCard(
+                        title = "",
+                        description = "",
+                        imageUrl = ""
+                    )
                 }
             }
         }
@@ -65,6 +93,7 @@ fun ItemScreen(
 fun ItemScreenPreview() {
     ItemScreen(
         navController = rememberNavController(),
-        lazyListState = rememberLazyListState()
+        lazyListState = rememberLazyListState(),
+        isTopAppBarVisible = true
     )
 }
