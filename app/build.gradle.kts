2f3f0f2ca6e1db67ed5ce05340580e079d10b855
Mario <marioluengo3d@gmail.com>
2024-10-30 21:07:13 +0100

    update compile and targetsdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.mluengo.memoryorganizer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mluengo.memoryorganizer"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
